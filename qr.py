fb31353c241195fe533503b1a84be46d2f44b9fc
tmmmmt <109069203+tmmmmt@users.noreply.github.com>
2022-07-11 20:36:09 +0900

    Add files via upload

--- qr.py
+++ qr.py
@@ -2,7 +2,6 @@ import streamlit as st
 import qrcode
 from PIL import Image
 from pyzbar.pyzbar import decode
-import cv2
 
 st.title('QRコード生成＆読み込みアプリ')
 url = st.text_input('URL入力')
@@ -14,17 +13,5 @@ if st.button('QRコード生成'):
     img = Image.open(fm)
     st.image(img)
 if st.button('QRコード読み込み'):
-	cap = cv2.VideoCapture(0)
-	font = cv2.FONT_HERSHEY_SIMPLEX
-	while cap.isOpened():
-		ret,frame = cap.read()
-		if ret == True:
-			d = decode(frame)
-			if d:
-				frame = cv2.putText(frame,d[0].data.decode('utf-8'),(10,50),font,1,(0,255,255),2,cv2.LINE_AA)
-			cv2.imshow('frame',frame)
-
-		if cv2.waitKey(1) & 0xFF == ord('q'):
-			break
-
-	cap.release()
+    img = Image.open(fm)
+    st.text(decode(img[0].data.decode('utf-8')))
