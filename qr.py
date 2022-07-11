8e7d633c38fe066401c93f693b4c42712285ebc0
tmmmmt <109069203+tmmmmt@users.noreply.github.com>
2022-07-11 19:42:39 +0900

    Add files via upload

--- qr.py
+++ qr.py
@@ -1,4 +1,14 @@
 import streamlit as st
+import qrcode
+from PIL import Image
+import cv2
 
 st.title('QRコード生成＆読み込みアプリ')
 url = st.text_input('URL入力')
+fm = 'qrcode.png'
+
+if st.button('QRコード生成'):
+    _img = qrcode.make(url)
+    _img.save(fm)
+    img = Image.open(fm)
+    st.image(img)
